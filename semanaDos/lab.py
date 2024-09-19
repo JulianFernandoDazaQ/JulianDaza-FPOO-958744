fd24619c462abb57d2746daa709902b7855166a8
Julian Daza <julian.daza-Q@uniminuto.edu.co>
2024-09-19 00:15:12 +0000

    lavoratorio semana dos

---
+++ semanaDos/lab.py
@@ -0,0 +1,26 @@
+
+person = {
+  "name ": "julian",
+  "age": 33
+
+}
+
+print(type(person["age"]))
+
+
+# product
+# name :str
+
+
+
+id = int(intput("porfavor ingrese el id de su producto'/"))
+name = input("porfavor ingrese el nmbre del producto")
+prince = float(input("porfavor ingrese el nombre del producto"))
+section = input (" porfavor ingrese la seccion para su producto")
+
+product1 ={
+  "id": id,
+  "name": name,
+  " prince": prince,
+  "section": section  
+}
