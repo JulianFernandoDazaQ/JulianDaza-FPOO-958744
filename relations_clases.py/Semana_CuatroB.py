e9c68ca0701f64f257d5e64e7158ddcfb0ce39b6
Julian Daza <julian.daza-Q@uniminuto.edu.co>
2024-10-06 17:42:41 +0000

    tareas de los diccionarios

---
+++ relations_clases.py/Semana_CuatroB.py
@@ -0,0 +1,19 @@
+class Author
+    def__ini__( self, name, books):
+       self.name = name
+       self.books = add_books
+
+
+    def __str__(self):
+        return F"{self.name}"
+
+class book:
+    def __init__( self, title: str, pages: int, author: Author):
+        self.title = title
+        self.pages = pages
+        self.author = author
+    def __repr__               
+
+author_1 = author("Gabriel" [])
+
+print(author_1)
